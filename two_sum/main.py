825b96c183e204ec2ec666dfdf5f378a75901dc3
Ramone Robertson <ramonerobertson10@gmail.com>
2026-07-09 10:35:53 -0500

    Finished brute force solution

--- two_sum/main.py
+++ two_sum/main.py
@@ -1,8 +1,17 @@
-def twoSum(self, nums: List[int], target: int) -> List[int]:
+def twoSum(nums: List[int], target: int) -> List[int]:
     # inputs - list of unique numbers (list) : target num
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        for j in range(i+1, len(nums)):
+            if nums[j] == complement:
+                return (i, j)
 
-    # loop through the list
-    # Evaluate what the complement is based on the target and current num in the iteration
-    # on each iteration check if the total adds up to the target
-    # return the indices in a list
-    pass
+test_case1 = [2,7,11,15]
+test_case2 = [3,2,4]
+test_case3 = [3,3]
+
+
+
+print(f"Test Case 1:\n Expected output: [0, 1]\n Actual Output: {twoSum(test_case1, 9)} ")
+print(f"Test Case 2:\n Expected output: [1, 2]\n Actual Output: {twoSum(test_case2, 6)}")
+print(f"Test Case 3:\n Expected output: [0, 1]\n Actual Output: {twoSum(test_case3, 6)}")
