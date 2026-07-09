c778a2c18c08889c7a667502fe38c3ba646b9737
Ramone Robertson <ramonerobertson10@gmail.com>
2026-07-09 10:07:51 -0500

    Added comments

---
+++ two_sum/main.py
@@ -0,0 +1,8 @@
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    # inputs - list of unique numbers (list) : target num
+
+    # loop through the list
+    # Evaluate what the complement is based on the target and current num in the iteration
+    # on each iteration check if the total adds up to the target
+    # return the indices in a list
+    pass
