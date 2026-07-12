e6d9ede513362a366af68238c9fdb849615b2496
Ramone Robertson <ramonerobertson10@gmail.com>
2026-07-12 12:28:18 -0500

    Started and solve palindrome question

---
+++ arrays_strings/palindrome/main.py
@@ -0,0 +1,18 @@
+def is_palindrome(text: str) -> bool:
+    left = 0
+    right = len(text) - 1
+
+    while left < right:
+        if text[left] != text[right]:
+            return False
+
+        left +=1
+        right -= 1
+
+    return True
+
+test_case1 = "racecar"
+test_case2 = "aceba"
+
+print(is_palindrome(test_case1)) # Expect True
+print(is_palindrome(test_case2)) # Expect False
